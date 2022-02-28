38e71daba13335497e07298e59847ab5fcb803c7
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2022-02-28 12:53:39 +0300

    homework2 done

--- core/src/com/star/app/game/Asteroid.java
+++ core/src/com/star/app/game/Asteroid.java
@@ -4,57 +4,96 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.star.app.game.helpers.Poolable;
 
 import static com.star.app.screen.ScreenManager.SCREEN_HEIGHT;
 import static com.star.app.screen.ScreenManager.SCREEN_WIDTH;
 
-public class Asteroid {
+public class Asteroid implements Poolable {
+    private final float RADIUS = 256.0f / 2.0f;
+    private GameController gc;
     private Texture texture;
     private Vector2 position;
     private Vector2 velocity;
     private float angle;
     private float rotationSpeed;
+    private float scale;
+    private boolean active;
 
-    public Asteroid() {
+    public Asteroid(GameController gc) {
+        this.gc = gc;
         this.texture = new Texture("asteroid.png");
-        this.position = new Vector2(MathUtils.random(-200, SCREEN_WIDTH + 200), MathUtils.random(-200, SCREEN_HEIGHT + 200));
-        this.velocity = new Vector2(MathUtils.random(-500, 500), MathUtils.random(-500, 500));
-        this.angle = MathUtils.random(0.0f, 360.0f);
-        this.rotationSpeed = MathUtils.random(-180.0f, 180.0f);
+        this.position = new Vector2(0, 0);
+        this.velocity = new Vector2(0, 0);
+        this.active = false;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
+    }
+
+    public Float getAngle() {
+        return angle;
+    }
+
+    public Float getRadius() {
+        return RADIUS;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    @Override
+    public boolean isActive() {
+        return active;
+    }
+
+    public void deactivate() {
+        active = false;
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, position.x - 128, position.y - 128, 128, 128,
-                256, 256, 1, 1, angle, 0, 0, 256, 256, false, false);
+        batch.draw(texture, position.x - RADIUS, position.y - RADIUS, RADIUS, RADIUS,
+                RADIUS * 2.0f, RADIUS * 2.0f, scale, scale, angle, 0, 0, (int) RADIUS * 2, (int) RADIUS * 2, false, false);
     }
 
+
     public void update(float dt) {
+        position.mulAdd(velocity, dt);
         angle += rotationSpeed * dt;
 
-        position.x += velocity.x * dt;
-        position.y += velocity.y * dt;
-
-        // при выходе за пределы параметров ScreenManager обновляются параметры угла атаки (направления), скорости и частоты вращения астероида. Рефакторим переопределение параметров через отдельный метод
+        // при выходе за пределы параметров ScreenManager обновляются параметры угла атаки (направления), скорости, частоты вращения и размера астероида. Рефакторим переопределение параметров через отдельный метод
         if (position.x < -200) {
             position.x = SCREEN_WIDTH + 200;
             position.y = MathUtils.random(0, SCREEN_HEIGHT);
             updateAttackAngleAndVelocityAndRotationSpeed();
+            // добавлять новый астероид, если текущий ушел за границы скринмэнеджера.
+            // Жесть конечно получается, надо придумать ограничения какие-нибудь.
+//            gc.renderAsteroids();
         }
         if (position.x > SCREEN_WIDTH + 200) {
             position.x = -200;
             position.y = MathUtils.random(0, SCREEN_HEIGHT);
             updateAttackAngleAndVelocityAndRotationSpeed();
+//            gc.renderAsteroids();
         }
 
         if (position.y < -200) {
             position.y = SCREEN_HEIGHT + 200;
             position.x = MathUtils.random(0, SCREEN_WIDTH);
             updateAttackAngleAndVelocityAndRotationSpeed();
+//            gc.renderAsteroids();
         }
         if (position.y > SCREEN_HEIGHT + 200) {
             position.y = -200;
             position.x = MathUtils.random(0, SCREEN_WIDTH);
             updateAttackAngleAndVelocityAndRotationSpeed();
+//            gc.renderAsteroids();
         }
     }
 
@@ -63,5 +102,14 @@ public class Asteroid {
         velocity.y = MathUtils.random(-500, 500);
         angle = MathUtils.random(0.0f, 360.0f);
         rotationSpeed = MathUtils.random(-180.0f, 180.0f);
+        scale = MathUtils.random(0.3f, 1.0f);
+    }
+
+    public void activate(float x, float y, float vx, float vy) {
+        this.position.set(x, y);
+        this.velocity.set(vx, vy);
+        this.angle = MathUtils.random(0.0f, 360.0f);
+        this.rotationSpeed = MathUtils.random(-180.0f, 180.0f);
+        this.active = true;
     }
 }
--- core/src/com/star/app/game/GameController.java
+++ core/src/com/star/app/game/GameController.java
@@ -1,19 +1,38 @@
 package com.star.app.game;
 
+import com.badlogic.gdx.math.MathUtils;
+
+import static com.star.app.screen.ScreenManager.SCREEN_HEIGHT;
+import static com.star.app.screen.ScreenManager.SCREEN_WIDTH;
+
 public class GameController {
 
     private Background background;
+    private AsteroidController ac;
     private BulletController bc;
     private Hero hero;
 
     public GameController() {
         this.background = new Background(this);
+        this.ac = new AsteroidController(this);
         this.bc = new BulletController();
         this.hero = new Hero(this);
+        renderAsteroids(10);
+    }
+
+    public void renderAsteroids(int count) {
+        for (int i = 0; i < count; i++) {
+            ac.setup(MathUtils.random(-200, SCREEN_WIDTH + 200), MathUtils.random(-200, SCREEN_HEIGHT + 200),
+                    MathUtils.random(-500, 500), MathUtils.random(-500, 500));
+        }
     }
 
     public Background getBackground() {
         return background;
+    }
+
+    public AsteroidController getAsteroidController() {
+        return ac;
     }
 
     public Hero getHero() {
@@ -26,6 +45,7 @@ public class GameController {
 
     public void update(float dt) {
         background.update(dt);
+        ac.update(dt);
         bc.update(dt);
         hero.update(dt);
         checkCollisions();
@@ -34,9 +54,15 @@ public class GameController {
     public void checkCollisions() {
         for (int i = 0; i < bc.getActiveList().size(); i++) {
             Bullet b = bc.getActiveList().get(i);
-
-            if (hero.getPosition().dst(b.getPosition()) < 32.0f) {
-                //b.deactivate();
+            for (int j = 0; j < ac.getActiveList().size(); j++) {
+                Asteroid a = ac.getActiveList().get(j);
+                // Если разнокалиберные камни, то можно вот так неравенство оформить,
+                // предварительно добавив константу радиуса (ну, можно и нативно прописать),
+                // геттер на нее и зарандомить масштаб, просто ради эксперемента сделал.
+                if (a.getPosition().dst(b.getPosition()) < a.getRadius() * a.getScale()) {
+                    b.deactivate();
+                    a.deactivate();
+                }
             }
         }
     }
